01616622a14258c8783ce5fc0992f3fdba504560
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-10-11 23:07:47 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <vector>
 #include <string>
+#include <fstream>
 
 void duplication_check(std::vector<std::string> controls_vector)
 {
@@ -19,12 +20,12 @@ void duplication_check(std::vector<std::string> controls_vector)
     }
 }
 
-int main()
+int test()
 {
     std::vector<std::string> controls_vector;
     std::string constructed_text;
     std::string user_input;
-    std::cout << "Enter text: " << "\n";
+    std::cout << "Enter text: (Type 'qqqq' to enter text | Type 'exit' to exit program safely;" << "\n";
     while (std::getline(std::cin, user_input))
     {
         /*
@@ -33,21 +34,24 @@ int main()
             break;
         }
         */
-        if (user_input == "exit")
+        if (user_input == "qqqq")
         {
             break;
+        }
+        else if (user_input == "exit")
+        {
+            return 1;
         }
         constructed_text += user_input;
     }
     std::cout << "\n";
     //std::cout << input << "\n";
-    // TODO: Break when ints located in string.
+    // TODO: Break when 4 digit numbers are not located in string.
     if (constructed_text.find_first_of("0123456789") == std::string::npos)
     {
-        std::cout << "[-] No numbers found in following text. Please try again:" << "\n";
+        std::cout << "[-] No 4 digit number found in following text. Please try again:" << "\n";
         std::cout << "[!] END" << "\n";
         std::cout << "[!] Exiting..." << "\n\n";
-        system("pause");
         return 0;
     }
     std::string temp_store;
@@ -61,24 +65,46 @@ int main()
             i = i + 4;
         }
     }
+    std::ofstream output_file;
+    //if (std::filesystem::exists("bookmark_record.csv") == false)
+    output_file.open("RISK.csv", std::ios::app);
+    std::cout << "[+] Opened RISK.csv successfully;" << "\n";
+    // Comma used as seperator in csv files.
     std::cout << "Result: " << "\n";
     for (int i = 0; i <= controls_vector.size() - 1; i++)
     {
+        if (i == 0)
+        {
+            output_file << "Control(s) Entry:,";
+        }
         if (i != controls_vector.size() - 1)
         {
             std::cout << controls_vector[i] << ",";
+            output_file << controls_vector[i] << ",";
         }
         else
         {
             std::cout << controls_vector[i];
+            output_file << controls_vector[i];
         }
     }
+    output_file << "\n\n";
+    output_file.close();
     std::cout << "\n\n";
     duplication_check(controls_vector);
-    std::cout << "\n\n";
+    std::cout << "\n";
+    return 0;
+}
+
+int main()
+{
+    int flag = 0;
+    while (flag == 0)
+    {
+        flag = test();
+    }
     std::cout << "[!] END" << "\n";
     std::cout << "[!] Exiting..." << "\n\n";
     system("pause");
     return 0;
-
 }
